f0e9cfa3b205ac7ec3e572e9dab288bd55d90716
jfisher8 <joefisher2@outlook.com>
2024-03-13 19:53:54 +0000

    adds button component for login and signup screens

---
+++ lib/widgets/login_signup_widgets.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+Container signUpButton(BuildContext context, bool isLogin, Function onTap) {
+  return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+      child: ElevatedButton(
+        onPressed: () {
+          onTap();
+        },
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return Colors.green[700];
+              }
+              return Colors.green;
+            }),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)))),
+        child: const Text('Sign Up'),
+      ));
+}
+
+Container loginButton(BuildContext context, bool isLogin, Function onTap) {
+  return Container(
+    width: MediaQuery.of(context).size.width,
+    height: 50,
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+    child: ElevatedButton(
+      onPressed: () {
+        onTap();
+      },
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.pressed)) {
+              return Colors.green[700];
+            }
+            return Colors.green;
+          }),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          )),
+      child: const Text('Login'),
+    ),
+  );
+}
--- lib/widgets/text_fields.dart
+++ lib/widgets/text_fields.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-TextFormField emailAddressTextField(String text, IconData icon, bool isPasswordType, TextEditingController controller) {
+TextFormField emailAddressTextField(String text, IconData icon,
+    bool isPasswordType, TextEditingController controller) {
   return TextFormField(
     key: const Key("emailAddress"),
     controller: controller,
@@ -10,44 +11,48 @@ TextFormField emailAddressTextField(String text, IconData icon, bool isPasswordT
     cursorColor: Colors.black,
     style: const TextStyle(color: Colors.black, fontSize: 14),
     decoration: InputDecoration(
-      prefixIcon: Icon(
-        icon, color: const Color.fromRGBO(56, 182, 255, 1.0),
-      ),
-      labelText: text,
-      labelStyle: GoogleFonts.mavenPro(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black),
-      filled: true,
-      floatingLabelBehavior: FloatingLabelBehavior.never,
-      fillColor: Colors.grey[400],
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30.0),
-      )
-    ),
+        prefixIcon: Icon(
+          icon,
+          color: const Color.fromRGBO(56, 182, 255, 1.0),
+        ),
+        labelText: text,
+        labelStyle: GoogleFonts.mavenPro(
+            fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black),
+        filled: true,
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        fillColor: Colors.grey[400],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        )),
   );
 }
 
-TextFormField usernameTextField(String text, IconData icon, bool isPasswordType, TextEditingController controller) {
+TextFormField usernameTextField(String text, IconData icon, bool isPasswordType,
+    TextEditingController controller) {
   return TextFormField(
     controller: controller,
     obscureText: isPasswordType,
     cursorColor: Colors.black,
     style: const TextStyle(color: Colors.black, fontSize: 14),
     decoration: InputDecoration(
-      prefixIcon: Icon(
-        icon, color: const Color.fromRGBO(56, 182, 255, 1.0),
-      ),
-      labelText: text,
-      labelStyle: GoogleFonts.mavenPro(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black),
-      filled: true,
-      floatingLabelBehavior: FloatingLabelBehavior.never,
-      fillColor: Colors.grey[400],
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30.0),
-      )
-    ),
+        prefixIcon: Icon(
+          icon,
+          color: const Color.fromRGBO(56, 182, 255, 1.0),
+        ),
+        labelText: text,
+        labelStyle: GoogleFonts.mavenPro(
+            fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black),
+        filled: true,
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        fillColor: Colors.grey[400],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        )),
   );
 }
 
-TextFormField passwordTextField(String text, IconData icon, bool isPasswordType, TextEditingController controller) {
+TextFormField passwordTextField(String text, IconData icon, bool isPasswordType,
+    TextEditingController controller) {
   return TextFormField(
     controller: controller,
     //validator: validatePassword, <-- TODO: uncomment when the password validation function is written
@@ -55,18 +60,21 @@ TextFormField passwordTextField(String text, IconData icon, bool isPasswordType,
     cursorColor: Colors.black,
     style: const TextStyle(color: Colors.black, fontSize: 14),
     decoration: InputDecoration(
-      prefixIcon: Icon(
-        icon, color: const Color.fromRGBO(56, 182, 255, 1.0),
-      ),
-      labelText: text,
-      labelStyle: GoogleFonts.mavenPro(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black),
-      filled: true,
-      floatingLabelBehavior: FloatingLabelBehavior.never,
-      fillColor: Colors.grey[400],
-      border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(30.0),
-      )
-    ),
-    keyboardType: isPasswordType ? TextInputType.visiblePassword : TextInputType.emailAddress,
+        prefixIcon: Icon(
+          icon,
+          color: const Color.fromRGBO(56, 182, 255, 1.0),
+        ),
+        labelText: text,
+        labelStyle: GoogleFonts.mavenPro(
+            fontSize: 12, fontWeight: FontWeight.bold, color: Colors.black),
+        filled: true,
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+        fillColor: Colors.grey[400],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        )),
+    keyboardType: isPasswordType
+        ? TextInputType.visiblePassword
+        : TextInputType.emailAddress,
   );
 }
