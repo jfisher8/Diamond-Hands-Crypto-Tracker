f4887a3cdd8483dab71e1ba7a9ce809ff9a2a38e
jfisher8 <joefisher2@outlook.com>
2024-03-27 23:23:02 +0000

    Firebase key testing

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+//import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'dart:developer' as developer;
 
 bool onboarded = true;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(
         options: const FirebaseOptions(  
           apiKey: "AIzaSyABkZc2EC8iY2vshGnv2U_NSYwz6-o-XrE",
