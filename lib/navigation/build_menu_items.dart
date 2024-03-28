a3ec29c9c3aa27011fc876b62af0573c179bd5b8
jfisher8 <joefisher2@outlook.com>
2024-03-28 17:54:04 +0000

    adds login logout styling logic to nav menu

--- lib/navigation/build_menu_items.dart
+++ lib/navigation/build_menu_items.dart
@@ -4,8 +4,12 @@ import 'package:diamond_hands_crypto_tracker/core_pages/latest_crypto_news.dart'
 import 'package:diamond_hands_crypto_tracker/core_pages/crypto_exchanges.dart';
 import 'package:diamond_hands_crypto_tracker/core_pages/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:developer' as developer;
 
 Widget buildMenuItems(BuildContext context) {
+  final String? currentEmail = FirebaseAuth.instance.currentUser?.email;
   return Container(
     padding: const EdgeInsets.all(24),
     child: Wrap(
@@ -24,36 +28,62 @@ Widget buildMenuItems(BuildContext context) {
           leading: const Icon(Icons.money_rounded),
           title: const Text('Latest Crypto Prices'),
           onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const LatestCryptoPrices()));
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const LatestCryptoPrices()));
           },
         ),
         ListTile(
           leading: const Icon(Icons.newspaper_rounded),
           title: const Text('Latest Crypto News'),
           onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const LatestCryptoNews()));
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const LatestCryptoNews()));
           },
         ),
         ListTile(
           leading: const Icon(Icons.currency_exchange_rounded),
           title: const Text('Crypto Exchanges'),
           onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const CryptoExchanges()));
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const CryptoExchanges()));
           },
         ),
         const Divider(color: Colors.black),
-        ListTile(
-          leading: const Icon(Icons.login_rounded, color: Colors.black),
-          title: const Text('Login'),
-          onTap: () {
-            Navigator.push(context,
-            //TODO: Change the following link to logout/logout screen when it's built
-                MaterialPageRoute(builder: (context) => const LoginScreen()));
-          },
-        ),
+        currentEmail != null
+            ? //if someone's LOGGED IN
+            ListTile(
+                leading: const Icon(Icons.logout_rounded, color: Colors.black),
+                title: const Text('Logout'),
+                onTap: () {
+                  FirebaseAuth.instance.signOut().then((value) async {
+                    SharedPreferences preferences =
+                        await SharedPreferences.getInstance();
+                    preferences.remove('emailAddress');
+                    developer.log('signed out of account;');
+                  });
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomeScreen()));
+                },
+              )
+            : //if nobody is logged in, show default list tile
+            ListTile(
+                leading: const Icon(Icons.login_rounded, color: Colors.black),
+                title: const Text('Login'),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginScreen()));
+                },
+              ),
       ],
     ),
   );
