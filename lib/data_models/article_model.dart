3c604403339ab33d57d0ee5b12afdcbdb5db6538
jfisher8 <joefisher2@outlook.com>
2024-03-29 22:58:17 +0000

    adds news article data model

---
+++ lib/data_models/article_model.dart
@@ -0,0 +1,28 @@
+import 'source_model.dart';
+
+class Article {
+  Source source;
+  String author;
+  String title;
+  String url;
+  String imageURL;
+  String publishedAt;
+
+  Article(
+      {required this.source,
+      required this.author,
+      required this.title,
+      required this.url,
+      required this.imageURL,
+      required this.publishedAt});
+
+  factory Article.fromJson(Map<String, dynamic> json) {
+    return Article(
+        source: Source.fromJson(json['source']),
+        author: json['author'],
+        title: json['title'],
+        url: json['url'],
+        imageURL: json['urlToImage'],
+        publishedAt: json['publishedAt']);
+  }
+}
