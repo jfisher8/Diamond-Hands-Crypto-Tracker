d58ea16831ac58e47904c23f78ee38c9b789649a
jfisher8 <joefisher2@outlook.com>
2024-03-29 19:39:02 +0000

    adds exchanges data model

---
+++ lib/data_models/exchanges_model.dart
@@ -0,0 +1,17 @@
+class Exchanges {
+  late String name;
+  late int yearEstablished;
+  late String url;
+  late String image;
+
+  Exchanges({
+    required this.name, required this.yearEstablished, required this.url, required this.image
+  });
+
+  Exchanges.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    yearEstablished = json['year_established'];
+    url = json['url'];
+    image = json['image'];
+  }
+}
