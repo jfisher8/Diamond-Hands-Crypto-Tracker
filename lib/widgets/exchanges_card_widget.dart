745a6832dba237280b502922ed8ed74cec47eb3d
jfisher8 <joefisher2@outlook.com>
2024-04-01 23:50:41 +0100

    adds required data class for exchanges card widget

---
+++ lib/widgets/exchanges_card_widget.dart
@@ -0,0 +1,24 @@
+import 'package:diamond_hands_crypto_tracker/data_models/exchanges_model.dart';
+import 'package:flutter/material.dart';
+//TODO add cached network image package
+//TODO add url launcher package if needed
+
+class ExchangesCard extends StatelessWidget {
+  ExchangesCard({
+    super.key,
+    required this.name,
+    required this.yearEstablished,
+    required this.url,
+    required this.image
+  });
+
+  String name;
+  String yearEstablished;
+  String url;
+  String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
