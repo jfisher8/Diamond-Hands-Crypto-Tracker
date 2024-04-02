0a2c26e1243e45af4bcd9fbf10ccf56033872e49
jfisher8 <joefisher2@outlook.com>
2024-04-02 17:57:50 +0100

    adds crypto exchanges list tile UI

--- lib/widgets/exchanges_card_widget.dart
+++ lib/widgets/exchanges_card_widget.dart
@@ -1,16 +1,14 @@
-import 'package:diamond_hands_crypto_tracker/data_models/exchanges_model.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-//TODO add cached network image package
 //TODO add url launcher package if needed
 
 class ExchangesCard extends StatelessWidget {
-  ExchangesCard({
-    super.key,
-    required this.name,
-    required this.yearEstablished,
-    required this.url,
-    required this.image
-  });
+  ExchangesCard(
+      {super.key,
+      required this.name,
+      required this.yearEstablished,
+      required this.url,
+      required this.image});
 
   String name;
   String yearEstablished;
@@ -19,9 +17,28 @@ class ExchangesCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
-    child: Card(
-      child: ListTile(),
-    ));
+    return Padding(
+        padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
+        child: Card(
+          child: ListTile(
+            onTap: () {
+              //add logic to open url to the exchange's website here
+            },
+            trailing: const Icon(Icons.open_in_new_rounded),
+            leading: ConstrainedBox(
+              constraints: const BoxConstraints(
+                  minWidth: 66, minHeight: 66, maxWidth: 66, maxHeight: 100),
+              child: CachedNetworkImage(
+                  imageUrl: image,
+                  placeholder: (imageUrl, error) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, imageUrl, error) => const Icon(
+                      Icons.error_outline_rounded,
+                      color: Colors.red)),
+            ),
+            title: Text(name),
+            subtitle: Text(yearEstablished),
+          ),
+        ));
   }
 }
