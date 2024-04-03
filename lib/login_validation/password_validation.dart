8c6fbf03bd0626194d27b7aa5422ca3c6045b9a4
jfisher8 <joefisher2@outlook.com>
2024-04-03 16:45:40 +0100

    fixes password validation on login & signup screens

--- lib/login_validation/password_validation.dart
+++ lib/login_validation/password_validation.dart
@@ -1,5 +1,5 @@
 String? validatePassword (String? formPassword) {
-  if (formPassword == null) {
+  if (formPassword == null || formPassword.isEmpty) {
     return 'Password cannot be empty';
   }
   return null;
