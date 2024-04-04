f1bcacc0588589e91a9465179ec5b1bd5c8a619d
jfisher8 <joefisher2@outlook.com>
2024-04-04 18:17:34 +0100

    attempts to add NewsAPI but snapshot has an error, needs fixing

--- lib/core_pages/latest_crypto_news.dart
+++ lib/core_pages/latest_crypto_news.dart
@@ -1,4 +1,4 @@
-import 'package:diamond_hands_crypto_tracker/api_functions/get_article_data.dart';
+import 'package:diamond_hands_crypto_tracker/widgets/news_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:diamond_hands_crypto_tracker/navigation/navigation_drawer.dart';
 import 'package:diamond_hands_crypto_tracker/widgets/appbar.dart';
@@ -6,7 +6,9 @@ import 'package:diamond_hands_crypto_tracker/core_pages/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:diamond_hands_crypto_tracker/core_pages/home_screen.dart';
+import 'dart:developer' as developer;
 import 'package:diamond_hands_crypto_tracker/data_models/article_model.dart';
+import 'package:diamond_hands_crypto_tracker/api_functions/get_article_data.dart';
 
 class LatestCryptoNews extends StatefulWidget {
   const LatestCryptoNews({super.key});
@@ -18,12 +20,13 @@ class LatestCryptoNews extends StatefulWidget {
 class _LatestCryptoNewsState extends State<LatestCryptoNews> {
   TextEditingController searchController = TextEditingController();
   late Future<List<Article>> futureArticle;
+  String searchString = "";
 
   @override
   void initState() {
-    super.initState();
     futureArticle = getArticleData();
-}
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,45 +62,51 @@ class _LatestCryptoNewsState extends State<LatestCryptoNews> {
         ],
       ),
       drawer: const NavigationMenu(),
-      body: Column(
-        children: <Widget>[
-          Padding(
-              padding: const EdgeInsets.all(5),
-              child: TextField(
-                controller: searchController,
-                decoration: const InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.blue, width: 2),
-                    )),
-                style: Theme.of(context).textTheme.bodySmall,
-              )),
-          Expanded(child: FutureBuilder(
-            future: futureArticle,
-            builder: (BuildContext context, snapshot) {
-              if (snapshot.hasData) {
-                List<Article> articles = snapshot.data!;
-                return ListView.builder(
-                  itemCount: articles.length,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Placeholder(); //replace with news list tile widget when it's made
+      body: Column(children: <Widget>[
+        Padding(
+            padding: const EdgeInsets.all(5),
+            child: TextField(
+              controller: searchController,
+              decoration: const InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue, width: 2),
+                  )),
+              style: Theme.of(context).textTheme.bodySmall,
+            )),
+            Expanded(
+            child: FutureBuilder(
+              future: futureArticle,
+              builder: (BuildContext context, AsyncSnapshot snapshot) {
+                if (snapshot.hasData) {
+                  List<Article> articles = snapshot.data!;
+                  developer.log(snapshot.data);
+                  return ListView.builder(
+                      itemCount: articles.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return articles[index]
+                                .source
+                                .name
+                                .contains(searchString)
+                            ? newsListTile(articles[index], context)
+                            : Container();
+                      });
+                }
+                if (snapshot.hasError) {
+                    developer.log('snapshot has an error');
                   }
-                  );
-              }
-              return const Column(
-                  children: [
-                    Padding(padding: EdgeInsets.all(100), child: Center(
-                      child: CircularProgressIndicator()),
-                    )
-                  ],
+                else {
+                return const Center(
+                  child: CircularProgressIndicator(),
                 );
-            }),
-
-          )]
-      ),
+              };
+              return const Center(child: CircularProgressIndicator());
+              }
+            ),
+      )]),
     );
   }
 }
