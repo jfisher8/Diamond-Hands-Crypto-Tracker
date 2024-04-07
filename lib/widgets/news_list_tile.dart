d785222ef1e660497e63383b806d9c950c2aa99f
jfisher8 <joefisher2@outlook.com>
2024-04-07 15:38:42 +0100

    adds sizedbox for NewsAPI images and adds ListTile underneath for article data and interactivity

--- lib/widgets/news_list_tile.dart
+++ lib/widgets/news_list_tile.dart
@@ -1,17 +1,42 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:diamond_hands_crypto_tracker/data_models/article_model.dart';
 
-Widget newsListTile (Article article, BuildContext context) {
-  return Padding(padding: 
-  const EdgeInsets.all(5),
-  child: Card(
-    child: ListTile(
-      onTap: () {
-        //add url launcher here if it's needed (or add link to the article detail page when developed)
-      },
-      title: Text(article.source.name),
-      subtitle: Text(article.title),
-      trailing: const Icon(Icons.arrow_forward_rounded),
-    ),
-  ));
+Widget newsListTile(Article article, BuildContext context) {
+  return Padding(
+      padding: const EdgeInsets.all(5),
+      child: Column(children: [
+        Card(
+            elevation: 30,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 200,
+                  child: CachedNetworkImage(
+                    imageUrl: article.imageURL.toString(),
+                    fit: BoxFit.fill,
+                    errorWidget: (context, url, error) => const Center(
+                        child: Icon(
+                      Icons.error_rounded,
+                      color: Colors.red,
+                    )),
+                  ),
+                ),
+                ListTile(
+                  title: Text(article.title.toString(),
+                      style: Theme.of(context).textTheme.bodyMedium),
+                  subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(article.source.name.toString(),
+                            style: Theme.of(context).textTheme.bodySmall),
+                        Text(article.publishedAt.toString(),
+                            style: Theme.of(context).textTheme.bodySmall)
+                      ]),
+                  trailing: const Icon(Icons.arrow_forward_rounded),
+                  onTap: () {},
+                )
+              ],
+            ))
+      ]));
 }
