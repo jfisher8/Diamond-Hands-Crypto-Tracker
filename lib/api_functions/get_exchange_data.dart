2fd6fd43590bbc0d0a4f63b5534c342161d5bfa6
jfisher8 <joefisher2@outlook.com>
2024-04-08 15:42:56 +0100

    adds API logic for exchanges data

---
+++ lib/api_functions/get_exchange_data.dart
@@ -0,0 +1,26 @@
+import 'package:diamond_hands_crypto_tracker/data_models/exchanges_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
+Future<List<Exchanges>> getExchangeData() async {
+  exchangesList = [];
+  final response = await http.get(Uri.parse('https://api.coingecko.com/api/v3/exchanges'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> exchanges = [];
+    exchanges = json.decode(response.body);
+    if (exchanges.isNotEmpty) {
+      for (int i = 0; i < exchanges.length; i++) {
+        if (exchanges[i] != null) {
+          Map<String, dynamic> map = exchanges[i];
+          exchangesList.add(Exchanges.fromJson(map));
+        }
+      }
+      return exchangesList;
+    } else {
+      throw Exception('Failed to load coins');
+    }
+  }
+  return exchangesList;
+}
