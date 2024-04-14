08042ef95dad777c22982d7e56c6060b22de9a9a
jfisher8 <joefisher2@outlook.com>
2024-04-14 23:07:10 +0100

    adds fetchExchanges function to crypto exchanges screen to try and decrease latency in API call; still needs a proper fix

--- lib/core_pages/crypto_exchanges.dart
+++ lib/core_pages/crypto_exchanges.dart
@@ -1,4 +1,6 @@
 import 'package:diamond_hands_crypto_tracker/api_functions/get_exchange_data.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:diamond_hands_crypto_tracker/data_models/exchanges_model.dart';
 import 'package:flutter/material.dart';
 import 'package:diamond_hands_crypto_tracker/navigation/navigation_drawer.dart';
@@ -68,3 +70,25 @@ class _CryptoExchangesState extends State<CryptoExchanges> {
         ));
   }
 }
+
+  Future<List<Exchanges>> fetchExchanges() async {
+    exchangesList = [];
+    final response =
+        await http.get(Uri.parse('https://api.coingecko.com/api/v3/exchanges'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> values = [];
+      values = json.decode(response.body);
+      if (values.isNotEmpty) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
+            Map<String, dynamic> map = values[i];
+            exchangesList.add(Exchanges.fromJson(map));
+          }
+        }
+      }
+      return exchangesList;
+    } else {
+      throw Exception('Failed to load coins');
+    }
+  }
