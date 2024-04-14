1f2ea6fa560b1540c933b6727994c7c582210297
jfisher8 <joefisher2@outlook.com>
2024-04-14 23:10:27 +0100

    adds fetchCoin to crypto prices screen logic to try and decrease API call latency when screen is loaded

--- lib/core_pages/latest_crypto_prices.dart
+++ lib/core_pages/latest_crypto_prices.dart
@@ -7,6 +7,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:diamond_hands_crypto_tracker/core_pages/favourites_screen.dart';
 import 'package:diamond_hands_crypto_tracker/data_models/coin_model.dart';
 import 'package:diamond_hands_crypto_tracker/api_functions/get_price_data.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class LatestCryptoPrices extends StatefulWidget {
   const LatestCryptoPrices({super.key});
@@ -16,6 +18,29 @@ class LatestCryptoPrices extends StatefulWidget {
 }
 
 class _LatestCryptoPricesState extends State<LatestCryptoPrices> {
+
+      Future<List<Coin>> fetchCoin() async {
+    coinList = [];
+    final response = await http.get(Uri.parse(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=50&page=1&sparkline=false'));
+
+    if (response.statusCode == 200) {
+      List<dynamic> values = [];
+      values = json.decode(response.body);
+      if (values.isNotEmpty) {
+        for (int i = 0; i < values.length; i++) {
+          if (values[i] != null) {
+            Map<String, dynamic> map = values[i];
+            coinList.add(Coin.fromJson(map));
+          }
+        }
+      }
+      return coinList;
+    } else {
+      //return coinList;
+      throw Exception('Failed to load coins');
+    }
+  }
 
   @override
   void initState() {
