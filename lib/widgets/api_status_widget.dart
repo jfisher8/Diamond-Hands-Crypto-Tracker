3f34fb96038ad09ee42a828195de0ce3c53316ac
jfisher8 <joefisher2@outlook.com>
2024-04-23 13:43:43 +0100

    adds unique coin error status widget component

--- lib/widgets/api_status_widget.dart
+++ lib/widgets/api_status_widget.dart
@@ -13,6 +13,19 @@ Widget buildLoadingCoinsStatus(BuildContext context) {
   );
 }
 
+Widget buildCoinsErrorStatus(BuildContext context) {
+  return const Center(
+    child: Column(
+      children: [
+        SizedBox(height: 40),
+        CircularProgressIndicator(),
+        SizedBox(height: 40),
+        Text('Error loading Coin data. Please try again later.'),
+      ],
+    ),
+  );
+}
+
 Widget buildDataErrorStatus(BuildContext context) {
   return const Center(
     child: Column(
