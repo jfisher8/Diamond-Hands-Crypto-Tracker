eacf78a625a70eb59f90d1781471f5b9cd7cc917
jfisher8 <joefisher2@outlook.com>
2024-05-20 21:20:15 +0100

    add developer log to reflect sharePrefs darkMode status

--- lib/themes/theme_provider.dart
+++ lib/themes/theme_provider.dart
@@ -1,6 +1,7 @@
 import 'package:diamond_hands_crypto_tracker/themes/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:developer' as developer;
 
 class ThemeProvider with ChangeNotifier {
   ThemeProvider() {
@@ -39,11 +40,13 @@ class ThemeProvider with ChangeNotifier {
       SharedPreferences darkModePreferences =
           await SharedPreferences.getInstance();
       await darkModePreferences.setBool('darkMode', true);
+      developer.log('Should the app be in dark mode? TRUE');
       themeData = darkMode;
     } else {
       SharedPreferences darkModePreferences =
           await SharedPreferences.getInstance();
       await darkModePreferences.setBool('darkMode', false);
+      developer.log('Should the app be in dark mode? FALSE');
       themeData = lightMode;
     }
   }
