bdda9c079098976ef19f11b2e3178196fa9ca070
jfisher8 <joefisher2@outlook.com>
2024-05-22 10:44:06 +0100

    switches new user onboarding & home screen order on startup if onboarding has been completed previously

--- lib/main.dart
+++ lib/main.dart
@@ -52,7 +52,7 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: onboarded == true ? NewUserOnboarding() : HomeScreen(),
+      home: onboarded == true ? const HomeScreen() : NewUserOnboarding(),
       theme: Provider.of<ThemeProvider>(context).themeData,
     );
   }
