40858a4c62e15cdbb2ecb855a0fed71fc4237917
jfisher8 <joefisher2@outlook.com>
2024-05-30 00:35:32 +0100

    update styling of prices & news subheadings on home screen for readability

--- lib/themes/themes.dart
+++ lib/themes/themes.dart
@@ -20,7 +20,7 @@ ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
     textTheme: TextTheme(
     titleSmall: GoogleFonts.questrial(fontWeight: FontWeight.bold, fontSize: 14, color: const Color.fromRGBO(56, 182, 255, 1.0)),
-    titleMedium: GoogleFonts.questrial(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.grey[400]),
+    titleMedium: GoogleFonts.questrial(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white),
     titleLarge: GoogleFonts.anton(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.black),
     bodySmall: GoogleFonts.mavenPro(fontWeight: FontWeight.bold, fontSize: 12, color: Colors.grey[350]),
     bodyMedium: GoogleFonts.mavenPro(fontWeight: FontWeight.bold, fontSize: 14, color: Colors.grey[350]),
