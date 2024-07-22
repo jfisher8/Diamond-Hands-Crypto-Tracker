dfe54adefb4e14196576ae52336573a9a44e9395
jfisher8 <joefisher2@outlook.com>
2024-07-22 17:04:00 +0100

    adjusts coin price styling in-widget

--- lib/widgets/coin_card_widget.dart
+++ lib/widgets/coin_card_widget.dart
@@ -83,7 +83,6 @@ class CoinCard extends StatelessWidget {
                   Text(
                     "£$price",
                     style: const TextStyle(
-                      color: Colors.white,
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
                     ),
