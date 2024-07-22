64eb9f40263c67e994db545de406547308f70d1c
jfisher8 <joefisher2@outlook.com>
2024-07-22 17:03:00 +0100

    switches coinCard widget from Container to Card

--- lib/widgets/coin_card_widget.dart
+++ lib/widgets/coin_card_widget.dart
@@ -23,13 +23,13 @@ class CoinCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 15, left: 10, right: 10),
-      child: Container(
-        height: 110,
-        decoration: BoxDecoration(
-          //TODO: explore adding dark mode check here to select colour of coin widget
-          color: Colors.grey[600],
-          borderRadius: BorderRadius.circular(20),
-        ),
+      child: Card(
+        // height: 110,
+        // decoration: BoxDecoration(
+        //   //TODO: explore adding dark mode check here to select colour of coin widget
+        //   color: Colors.grey[600],
+        //   borderRadius: BorderRadius.circular(20),
+        // ),
         child: Row(
           children: [
             Padding(
