b80a9b31b51d3e78160ac998fb0e535fc82ef1e8
jfisher8 <joefisher2@outlook.com>
2024-08-08 21:02:25 +0100

    adds image error handling to crypto exchanges details

--- lib/core_pages/crypto_exchanges_details.dart
+++ lib/core_pages/crypto_exchanges_details.dart
@@ -51,6 +51,7 @@ class CryptoExchangesDetails extends StatelessWidget {
                       child: CachedNetworkImage(
                         imageUrl: exchanges.imageURL,
                         placeholder: (context, url) => buildLoadingIcon(context),
+                        errorWidget: (context, url, error) => const Icon(Icons.error),
                         )),
                   const Padding(padding: EdgeInsets.all(5)),
                   Text(exchanges.name, style: const TextStyle(fontSize: 28)),
