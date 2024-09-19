59d0ca6edf2400386a8c42e29daf2354a54cc91d
jfisher8 <joefisher2@outlook.com>
2024-09-20 00:27:25 +0100

    adds the base UI for the cryptoExchangesReadMore component

--- lib/widgets/external_link_button_widgets.dart
+++ lib/widgets/external_link_button_widgets.dart
@@ -24,3 +24,31 @@ Container newsArticleReadMoreButton(BuildContext context, Function onTap) {
     ),
   );
 }
+
+
+//TODO: replicate the CoinCard widget to see if it's possible to dynamically add the
+//crypto exchange's name to the button component below
+Container cryptoExchangesReadMoreButton(BuildContext context, Function onTap) {
+  return Container(
+    width: 250,
+    height: 50,
+    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+    child: ElevatedButton(
+      onPressed: () {
+        onTap();
+      },
+      style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.resolveWith((states) {
+            if (states.contains(MaterialState.pressed)) {
+              return const Color.fromRGBO(56, 182, 255, 1.0);
+            }
+            return const Color.fromRGBO(56, 182, 255, 1.0);
+          }),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+          )),
+      child: Text('Learn more about the Exchange', style: Theme.of(context).textTheme.bodyLarge),
+    ),
+  );
+}
