68a9535f277e366e0e3198f3e5a8d3931c5a5734
jfisher8 <joefisher2@outlook.com>
2024-09-26 17:06:16 +0100

    removes original, unnecessary attempt at exchanges external link component

--- lib/widgets/external_link_button_widgets.dart
+++ lib/widgets/external_link_button_widgets.dart
@@ -36,51 +36,27 @@ class cryptoExchangesReadMoreButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-    width: 250,
-    height: 50,
-    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
-    child: ElevatedButton(
-      onPressed: () {
-        //link here
-      },
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.resolveWith((states) {
-            if (states.contains(MaterialState.pressed)) {
+      width: 250,
+      height: 50,
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+      child: ElevatedButton(
+        onPressed: () {
+          //link here
+        },
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return const Color.fromRGBO(56, 182, 255, 1.0);
+              }
               return const Color.fromRGBO(56, 182, 255, 1.0);
-            }
-            return const Color.fromRGBO(56, 182, 255, 1.0);
-          }),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          )),
-      child: Text('Learn more about ' + name.toString(), style: Theme.of(context).textTheme.bodyLarge),
-    ),
-  );
+            }),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            )),
+        child: Text('Learn more about ' + name.toString(),
+            style: Theme.of(context).textTheme.bodyLarge),
+      ),
+    );
   }
 }
-
-// Container cryptoExchangesReadMoreButton(BuildContext context, Function onTap) {
-//   return Container(
-//     width: 250,
-//     height: 50,
-//     margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-//     decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
-//     child: ElevatedButton(
-//       onPressed: () {
-//         onTap();
-//       },
-//       style: ButtonStyle(
-//           backgroundColor: MaterialStateProperty.resolveWith((states) {
-//             if (states.contains(MaterialState.pressed)) {
-//               return const Color.fromRGBO(56, 182, 255, 1.0);
-//             }
-//             return const Color.fromRGBO(56, 182, 255, 1.0);
-//           }),
-//           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-//             RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-//           )),
-//       child: Text('Learn more about the Exchange', style: Theme.of(context).textTheme.bodyLarge),
-//     ),
-//   );
-// }
