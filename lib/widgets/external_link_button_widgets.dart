cb5ccb11f3f4a86190ae9655e4036fea9b7d5f79
jfisher8 <joefisher2@outlook.com>
2024-10-04 09:10:38 +0100

    updates external link component icon styling

--- lib/widgets/external_link_button_widgets.dart
+++ lib/widgets/external_link_button_widgets.dart
@@ -63,7 +63,7 @@ class CryptoExchangesReadMoreButton extends StatelessWidget {
               Text('Learn more about $name',
                   style: GoogleFonts.mavenPro(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black)),
                   const Padding(padding: EdgeInsets.fromLTRB(5, 0, 0, 0)),
-                  const Icon(Icons.open_in_new_rounded, color: Colors.white)
+                  const Icon(Icons.open_in_new_rounded, color: Colors.black)
             ],
           ),
         ));
