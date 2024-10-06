4385ae68183e4d731ac2368c2af56a846080b4fc
jfisher8 <joefisher2@outlook.com>
2024-10-06 14:07:48 +0100

    updates coin api call to only pull and save current_price and image json items into Firebase

--- lib/api_functions/get_price_data.dart
+++ lib/api_functions/get_price_data.dart
@@ -1,28 +1,64 @@
-import 'package:diamond_hands_crypto_tracker/data_models/coin_model.dart';
-    import 'dart:convert';
-    import 'package:http/http.dart' as http;
-    import 'package:cloud_firestore/cloud_firestore.dart';
-    
-    Future<List<Coin>> fetchCoin() async {
-    coinList = [];
-    final response = await http.get(Uri.parse(
-        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=50&page=1&sparkline=false'));
+// import 'package:diamond_hands_crypto_tracker/data_models/coin_model.dart';
+//     import 'dart:convert';
+//     import 'package:http/http.dart' as http;
+//     import 'package:cloud_firestore/cloud_firestore.dart';
 
-    if (response.statusCode == 200) {
-      List<dynamic> values = [];
-      values = json.decode(response.body);
-      if (values.isNotEmpty) {
-        for (int i = 0; i < values.length; i++) {
-          if (values[i] != null) {
-            Map<String, dynamic> map = values[i];
-            coinList.add(Coin.fromJson(map));
-            FirebaseFirestore.instance.collection("coins").add(({"coins": values}));
-          }
+//     Future<List<Coin>> fetchCoin() async {
+//     coinList = [];
+//     final response = await http.get(Uri.parse(
+//         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=50&page=1&sparkline=false'));
+
+//     if (response.statusCode == 200) {
+//       List<dynamic> values = [];
+//       values = json.decode(response.body);
+//       if (values.isNotEmpty) {
+//         for (int i = 0; i < values.length; i++) {
+//           if (values[i] != null) {
+//             Map<String, dynamic> map = values[i];
+//             coinList.add(Coin.fromJson(map));
+//             FirebaseFirestore.instance.collection("coins").add(({"coins": values}));
+//           }
+//         }
+//       }
+//       return coinList;
+//     } else {
+//       //return coinList;
+//       throw Exception('Failed to load coins');
+//     }
+//   }
+
+import 'package:diamond_hands_crypto_tracker/data_models/coin_model.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<List<Coin>> fetchCoin() async {
+  List<Coin> coinList = [];
+  final response = await http.get(Uri.parse(
+      'https://api.coingecko.com/api/v3/coins/markets?vs_currency=gbp&order=market_cap_desc&per_page=50&page=1&sparkline=false'));
+
+  if (response.statusCode == 200) {
+    List<dynamic> values = json.decode(response.body);
+
+    if (values.isNotEmpty) {
+      for (var value in values) {
+        if (value != null) {
+          coinList.add(Coin.fromJson(value));
+
+          Map<String, dynamic> filteredData = {
+            "price": value["current_price"],
+            "image": value["image"],
+          };
+
+          await FirebaseFirestore.instance
+              .collection("coins")
+              .doc(value["id"])
+              .set(filteredData);
         }
       }
-      return coinList;
-    } else {
-      //return coinList;
-      throw Exception('Failed to load coins');
     }
+    return coinList;
+  } else {
+    throw Exception('Failed to load coins');
   }
+}
