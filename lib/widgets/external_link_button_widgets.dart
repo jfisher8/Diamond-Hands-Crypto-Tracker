48430ace5db8c1ec24847d97a5dbc2a0d7507f85
jfisher8 <joefisher2@outlook.com>
2024-10-10 17:20:32 +0100

    fixes 'read more' widget component text content

--- lib/widgets/external_link_button_widgets.dart
+++ lib/widgets/external_link_button_widgets.dart
@@ -4,38 +4,37 @@ import 'package:url_launcher/url_launcher.dart';
 
 Container newsArticleReadMoreButton(BuildContext context, Function onTap) {
   return Container(
-    width: 250,
-    height: 50,
-    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
-    child: ElevatedButton(
-      onPressed: () {
-        onTap();
-      },
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.resolveWith((states) {
-            if (states.contains(MaterialState.pressed)) {
+      width: 250,
+      height: 50,
+      margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
+      child: ElevatedButton(
+        onPressed: () {
+          onTap();
+        },
+        style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.resolveWith((states) {
+              if (states.contains(MaterialState.pressed)) {
+                return const Color.fromRGBO(56, 182, 255, 1.0);
+              }
               return const Color.fromRGBO(56, 182, 255, 1.0);
-            }
-            return const Color.fromRGBO(56, 182, 255, 1.0);
-          }),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-            RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-          )),
-    child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text('Learn more about $name',
-                  style: GoogleFonts.mavenPro(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
-                      color: Colors.black)),
-              const Padding(padding: EdgeInsets.fromLTRB(5, 0, 0, 0)),
-              const Icon(Icons.open_in_new_rounded,
-                  color: Colors.black, size: 20)
-            ],
-          ),
-  );
+            }),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            )),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text('Read more...',
+                style: GoogleFonts.mavenPro(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 14,
+                    color: Colors.black)),
+            const Padding(padding: EdgeInsets.fromLTRB(5, 0, 0, 0)),
+            const Icon(Icons.open_in_new_rounded, color: Colors.black, size: 20)
+          ],
+        ),
+      ));
 }
 
 class CryptoExchangesReadMoreButton extends StatelessWidget {
